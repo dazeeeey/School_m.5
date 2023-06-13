5e0b173236b1c050a4fe5503ef37f73493e6a87e
dayzeeeey <namokolo123@gmail.com>
2023-06-13 14:06:30 +0700

    .

--- week4.py
+++ week4.py
@@ -3,14 +3,22 @@ import cv2 as cv
 img = cv.imread("OIP.jpg",1)#color
 cv.imshow('image_color',img)
 
+
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 imageLine = img.copy()
-pointA = (200,80)
-pointB = (450,500)
-
+x,y =imageLine.shape[:2]
+print(x,y)
+y =int(y/2)
+x=int(x/2)
+circle_center = (y,x)
+# pointA = (200,80)
+# pointB = (450,500)
+radius = 100 
 # cv.arrowedLine(imageLine,pointA,pointB,(255,255,0),thickness=3)
-cv.rectangle(imageLine,pointA,pointB,(255,255,0),thickness=3) # tkicnes -1 =ทึบ
+# cv.rectangle(imageLine,pointA,pointB,(255,255,0),thickness=3) # tkicnes -1 =ทึบ
+cv.circle(imageLine,circle_center,radius,(0,0,255),thickness=3)#tkicnes -1 =ทึบ
+
 cv.imshow("Image line",imageLine)
 cv.waitKey(0)
