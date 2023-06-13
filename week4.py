a8ee794b8941d7c0b21a98cffd917c2930919617
dayzeeeey <namokolo123@gmail.com>
2023-06-13 13:50:33 +0700

    .

--- week4.py
+++ week4.py
@@ -8,8 +8,9 @@ cv.destroyAllWindows()
 
 imageLine = img.copy()
 pointA = (200,80)
-pointB = (450,80)
+pointB = (450,500)
 
-cv.arrowedLine(imageLine,pointA,pointB,(255,255,0),thickness=3)
+# cv.arrowedLine(imageLine,pointA,pointB,(255,255,0),thickness=3)
+cv.rectangle(imageLine,pointA,pointB,(255,255,0),thickness=3) # tkicnes -1 =ทึบ
 cv.imshow("Image line",imageLine)
 cv.waitKey(0)
